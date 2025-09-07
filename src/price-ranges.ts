599069e43f356359d21c8d4c4045cdfd34a0cb4d
itgiup <itgiup.com@gmail.com>
2025-09-07 20:27:27 +0700

    Fix: Correct price range handle movement

--- src/price-ranges.ts
+++ src/price-ranges.ts
@@ -1,19 +1,19 @@
 import {
 	AutoscaleInfo,
-	Logical,
-	Time,
-	SeriesAttachedParameter,
-	PrimitiveHoveredItem,
 	Coordinate,
+	IChartApi,
+	Logical,
 	MouseEventParams,
+	SeriesAttachedParameter,
+	Time,
 } from 'lightweight-charts';
 
 import { Point, PricerangesDataSource, InfoLabelData } from './data-source';
+import { ExternalId } from './helpers/constants';
 import { PricerangesOptions, defaultOptions } from './options';
 import { PricerangesPaneView } from './pane-view';
 import { PluginBase } from './plugin-base';
 
-// A static class to manage selection state across multiple instances
 class SelectionManager {
 	private static _selectedItem: Priceranges | null = null;
 
@@ -27,19 +27,22 @@ class SelectionManager {
 		}
 		if (this._selectedItem) {
 			this._selectedItem.setSelected(false);
+			this._selectedItem.setHovered(false);
 		}
 		this._selectedItem = item;
 		if (this._selectedItem) {
 			this._selectedItem.setSelected(true);
+			this._selectedItem.setHovered(true);
 		}
 	}
 }
 
-export class Priceranges
-	extends PluginBase
-	implements PricerangesDataSource
-{
-	private static _lastHoveredItem: Priceranges | null = null;
+export class Priceranges extends PluginBase implements PricerangesDataSource {
+	private static _instances: Priceranges[] = [];
+	private static _eventHandlerAttached: boolean = false;
+	private static _lastHoveredInstance: Priceranges | null = null;
+	private static _chart: IChartApi | null = null;
+	private static _stickyPart: { instance: Priceranges; part: string } | null = null;
 
 	private _options: PricerangesOptions;
 	p1: Point;
@@ -53,6 +56,7 @@ export class Priceranges
 	private _initialP1: Point | null = null;
 	private _initialP2: Point | null = null;
 	private _startDragLogicalPoint: Point | null = null;
+	private _activePricePoint: 'p1' | 'p2' | null = null;
 
 	public constructor(
 		p1: Point,
@@ -67,29 +71,62 @@ export class Priceranges
 			...options,
 		};
 		this._paneViews = [new PricerangesPaneView(this)];
+		Priceranges._instances.push(this);
 	}
 
 	public attached(param: SeriesAttachedParameter<Time>): void {
 		super.attached(param);
-		this.chart.subscribeClick(this._handleClick);
-		this.chart.subscribeCrosshairMove(this._handleCrosshairMove);
+		if (!Priceranges._eventHandlerAttached) {
+			Priceranges._chart = param.chart;
+			param.chart.subscribeClick(Priceranges._handleGlobalClick);
+			param.chart.subscribeCrosshairMove(Priceranges._handleGlobalCrosshairMove);
+			Priceranges._eventHandlerAttached = true;
+		}
 
 		const chartElement = this.chart.chartElement();
-		chartElement.addEventListener('mousedown', this._handleMouseDown);
-		chartElement.addEventListener('mouseup', this._handleMouseUp);
-		chartElement.addEventListener('mousemove', this._handleMouseMove);
-		chartElement.addEventListener('mouseleave', this._handleMouseLeave);
+		chartElement.addEventListener(
+			'mousedown',
+			this._handleMouseDown
+		);
+		chartElement.addEventListener(
+			'mouseup',
+			this._handleMouseUp
+		);
+		chartElement.addEventListener(
+			'mousemove',
+			this._handleMouseMove
+		);
+		chartElement.addEventListener(
+			'mouseleave',
+			this._handleMouseLeave
+		);
 	}
 
 	public detached(): void {
-		this.chart.unsubscribeClick(this._handleClick);
-		this.chart.unsubscribeCrosshairMove(this._handleCrosshairMove);
+		Priceranges._instances = Priceranges._instances.filter(p => p !== this);
+		if (Priceranges._instances.length === 0 && Priceranges._chart) {
+			Priceranges._chart.unsubscribeClick(Priceranges._handleGlobalClick);
+			Priceranges._chart.unsubscribeCrosshairMove(Priceranges._handleGlobalCrosshairMove);
+			Priceranges._eventHandlerAttached = false;
+		}
 
 		const chartElement = this.chart.chartElement();
-		chartElement.removeEventListener('mousedown', this._handleMouseDown);
-		chartElement.removeEventListener('mouseup', this._handleMouseUp);
-		chartElement.removeEventListener('mousemove', this._handleMouseMove);
-		chartElement.removeEventListener('mouseleave', this._handleMouseLeave);
+		chartElement.removeEventListener(
+			'mousedown',
+			this._handleMouseDown
+		);
+		chartElement.removeEventListener(
+			'mouseup',
+			this._handleMouseUp
+		);
+		chartElement.removeEventListener(
+			'mousemove',
+			this._handleMouseMove
+		);
+		chartElement.removeEventListener(
+			'mouseleave',
+			this._handleMouseLeave
+		);
 
 		super.detached();
 	}
@@ -120,10 +157,6 @@ export class Priceranges
 
 	paneViews() {
 		return this._paneViews;
-	}
-
-	public hitTest(x: Coordinate, y: Coordinate): PrimitiveHoveredItem | null {
-		return this._paneViews[0].hitTest(x, y);
 	}
 
 	public getInfoLabelData(): InfoLabelData | null {
@@ -172,44 +205,191 @@ export class Priceranges
 		this.requestUpdate();
 	}
 
-	private _handleClick = () => {
-		if (Priceranges._lastHoveredItem === this) {
-			if (SelectionManager.selectedItem === this) {
-				SelectionManager.selectedItem = null;
-			} else {
-				SelectionManager.selectedItem = this;
+	private static _handleGlobalClick = (param: MouseEventParams) => {
+		if (!param.point || !Priceranges._chart) return;
+
+		if (Priceranges._stickyPart) {
+			Priceranges._stickyPart = null;
+			Priceranges._chart.applyOptions({
+				handleScroll: { mouseWheel: true, pressedMouseMove: true },
+			});
+			return;
+		}
+
+		let clickedInstance: Priceranges | null = null;
+		let clickedPart: string | null = null;
+
+		for (const instance of Priceranges._instances) {
+			const hitResult = instance.paneViews()[0].hitTest(param.point.x, param.point.y);
+			if (hitResult) {
+				clickedInstance = instance;
+				clickedPart = hitResult.externalId;
+				break;
 			}
+		}
+
+		if (clickedInstance && clickedPart) {
+			if (clickedPart !== ExternalId.BODY) {
+				Priceranges._stickyPart = { instance: clickedInstance, part: clickedPart };
+				Priceranges._chart.applyOptions({
+					handleScroll: { mouseWheel: false, pressedMouseMove: false },
+				});
+
+				// New logic to set _activePricePoint
+				const currentMinPrice = Math.min(clickedInstance.p1.price, clickedInstance.p2.price);
+				const currentMaxPrice = Math.max(clickedInstance.p1.price, clickedInstance.p2.price);
+
+				switch (clickedPart) {
+					case ExternalId.TOP_HANDLE:
+						clickedInstance._activePricePoint = clickedInstance.p1.price === currentMaxPrice ? 'p1' : 'p2';
+						break;
+					case ExternalId.BOTTOM_HANDLE:
+						clickedInstance._activePricePoint = clickedInstance.p1.price === currentMinPrice ? 'p1' : 'p2';
+						break;
+					// For corner handles, we need to decide which point is being dragged for price.
+					// Assuming TOP_LEFT and TOP_RIGHT affect the top price, and BOTTOM_LEFT and BOTTOM_RIGHT affect the bottom price.
+					case ExternalId.TOP_LEFT_HANDLE:
+					case ExternalId.TOP_RIGHT_HANDLE:
+						clickedInstance._activePricePoint = clickedInstance.p1.price === currentMaxPrice ? 'p1' : 'p2';
+						break;
+					case ExternalId.BOTTOM_LEFT_HANDLE:
+					case ExternalId.BOTTOM_RIGHT_HANDLE:
+						clickedInstance._activePricePoint = clickedInstance.p1.price === currentMinPrice ? 'p1' : 'p2';
+						break;
+					default:
+						clickedInstance._activePricePoint = null; // Reset for other handles
+						break;
+				}
+
+			} else {
+				if (SelectionManager.selectedItem === clickedInstance) {
+					SelectionManager.selectedItem = null;
+				} else {
+					SelectionManager.selectedItem = clickedInstance;
+				}
+			}
+		} else {
+			SelectionManager.selectedItem = null;
 		}
 	};
 
-	private _handleCrosshairMove = (params: MouseEventParams) => {
-		if (this._isDragging) return;
-		const hoveredSource = params.hoveredObjectId as unknown as Priceranges | undefined;
-		if (hoveredSource && hoveredSource instanceof Priceranges) {
-			if (Priceranges._lastHoveredItem !== hoveredSource) {
-				if (Priceranges._lastHoveredItem) {
-					Priceranges._lastHoveredItem.setHovered(false);
-				}
-				Priceranges._lastHoveredItem = hoveredSource;
-				hoveredSource.setHovered(true);
+	private static _handleGlobalCrosshairMove = (param: MouseEventParams) => {
+		if (Priceranges._stickyPart && param.point) {
+			const { instance, part } = Priceranges._stickyPart;
+			const time = instance.chart.timeScale().coordinateToTime(param.point.x);
+			const price = instance.series.coordinateToPrice(param.point.y);
+			if (!time || !price) return;
+
+			const currentMinTime = Math.min(instance.p1.time as number, instance.p2.time as number);
+			const currentMaxTime = Math.max(instance.p1.time as number, instance.p2.time as number);
+
+			switch (part) {
+				case ExternalId.LEFT_HANDLE:
+					if (instance.p1.time === currentMinTime) {
+						instance.p1.time = time;
+					} else {
+						instance.p2.time = time;
+					}
+					break;
+				case ExternalId.RIGHT_HANDLE:
+					if (instance.p1.time === currentMaxTime) {
+						instance.p1.time = time;
+					} else {
+						instance.p2.time = time;
+					}
+					break;
+				case ExternalId.TOP_HANDLE:
+					if (instance._activePricePoint) {
+						instance[instance._activePricePoint].price = price;
+					}
+					break;
+				case ExternalId.BOTTOM_HANDLE:
+					if (instance._activePricePoint) {
+						instance[instance._activePricePoint].price = price;
+					}
+					break;
+				case ExternalId.TOP_LEFT_HANDLE:
+					if (instance.p1.time === currentMinTime) {
+						instance.p1.time = time;
+					} else {
+						instance.p2.time = time;
+					}
+					if (instance._activePricePoint) {
+						instance[instance._activePricePoint].price = price;
+					}
+					break;
+				case ExternalId.TOP_RIGHT_HANDLE:
+					if (instance.p1.time === currentMaxTime) {
+						instance.p1.time = time;
+					} else {
+						instance.p2.time = time;
+					}
+					if (instance._activePricePoint) {
+						instance[instance._activePricePoint].price = price;
+					}
+					break;
+				case ExternalId.BOTTOM_LEFT_HANDLE:
+					if (instance.p1.time === currentMinTime) {
+						instance.p1.time = time;
+					} else {
+						instance.p2.time = time;
+					}
+					if (instance._activePricePoint) {
+						instance[instance._activePricePoint].price = price;
+					}
+					break;
+				case ExternalId.BOTTOM_RIGHT_HANDLE:
+					if (instance.p1.time === currentMaxTime) {
+						instance.p1.time = time;
+					} else {
+						instance.p2.time = time;
+					}
+					if (instance._activePricePoint) {
+						instance[instance._activePricePoint].price = price;
+					}
+					break;
 			}
-		} else {
-			if (Priceranges._lastHoveredItem) {
-				Priceranges._lastHoveredItem.setHovered(false);
-				Priceranges._lastHoveredItem = null;
+			instance.requestUpdate();
+			return;
+		}
+
+		if (!param.point) {
+			if (Priceranges._lastHoveredInstance) {
+				Priceranges._lastHoveredInstance.setHovered(false);
+				Priceranges._lastHoveredInstance = null;
 			}
+			return;
+		}
+
+		let currentlyHovered: Priceranges | null = null;
+		for (const instance of Priceranges._instances) {
+			const hitResult = instance.paneViews()[0].hitTest(param.point.x, param.point.y);
+			if (hitResult) {
+				currentlyHovered = instance;
+				break;
+			}
+		}
+
+		if (Priceranges._lastHoveredInstance !== currentlyHovered) {
+			if (Priceranges._lastHoveredInstance) {
+				Priceranges._lastHoveredInstance.setHovered(false);
+			}
+			if (currentlyHovered) {
+				currentlyHovered.setHovered(true);
+			}
+			Priceranges._lastHoveredInstance = currentlyHovered;
 		}
 	};
 
 	private _handleMouseDown = (event: MouseEvent) => {
+		if (Priceranges._stickyPart) return;
 		const chartElement = this.chart.chartElement();
 		const rect = chartElement.getBoundingClientRect();
 		const x = event.clientX - rect.left;
 		const y = event.clientY - rect.top;
-		const hitTestResult = this.hitTest(x as Coordinate, y as Coordinate);
+		const hitTestResult = this.paneViews()[0].hitTest(x as Coordinate, y as Coordinate);
 
-		if (hitTestResult && hitTestResult.externalId) {
-			SelectionManager.selectedItem = this;
+		if (hitTestResult && hitTestResult.externalId === ExternalId.BODY) {
 			this._isDragging = true;
 			this._draggedPart = hitTestResult.externalId as string;
 			const time = this.chart.timeScale().coordinateToTime(x as Coordinate);
@@ -233,6 +413,7 @@ export class Priceranges
 		this._startDragLogicalPoint = null;
 		this._initialP1 = null;
 		this._initialP2 = null;
+		this._activePricePoint = null; // Reset active price point
 
 		this.chart.applyOptions({
 			handleScroll: {
@@ -244,6 +425,7 @@ export class Priceranges
 	private _handleMouseLeave = () => {
 		this._isDragging = false;
 		this._draggedPart = null;
+		this._activePricePoint = null; // Reset active price point
 	};
 
 	private _handleMouseMove = (event: MouseEvent) => {
@@ -256,45 +438,15 @@ export class Priceranges
 		const price = this.series.coordinateToPrice(y as Coordinate);
 		if (!time || !price) return;
 
-		switch (this._draggedPart) {
-			case 'left-handle':
-				this.p1.time = time;
-				break;
-			case 'right-handle':
-				this.p2.time = time;
-				break;
-			case 'top-handle':
-				this.p1.price = price;
-				break;
-			case 'bottom-handle':
-				this.p2.price = price;
-				break;
-			case 'top-left-handle':
-				this.p1.time = time;
-				this.p1.price = price;
-				break;
-			case 'top-right-handle':
-				this.p2.time = time;
-				this.p1.price = price;
-				break;
-			case 'bottom-left-handle':
-				this.p1.time = time;
-				this.p2.price = price;
-				break;
-			case 'bottom-right-handle':
-				this.p2.time = time;
-				this.p2.price = price;
-				break;
-			case 'body':
-				if (this._startDragLogicalPoint && this._initialP1 && this._initialP2) {
-					const timeDelta = (time as number) - (this._startDragLogicalPoint.time as number);
-					const priceDelta = price - this._startDragLogicalPoint.price;
-					this.p1.time = (this._initialP1.time as number) + timeDelta as Time;
-					this.p2.time = (this._initialP2.time as number) + timeDelta as Time;
-					this.p1.price = this._initialP1.price + priceDelta;
-					this.p2.price = this._initialP2.price + priceDelta;
-				}
-				break;
+		if (this._draggedPart === ExternalId.BODY) {
+			if (this._startDragLogicalPoint && this._initialP1 && this._initialP2) {
+				const timeDelta = (time as number) - (this._startDragLogicalPoint.time as number);
+				const priceDelta = price - this._startDragLogicalPoint.price;
+				this.p1.time = (this._initialP1.time as number) + timeDelta as Time;
+				this.p2.time = (this._initialP2.time as number) + timeDelta as Time;
+				this.p1.price = this._initialP1.price + priceDelta;
+				this.p2.price = this._initialP2.price + priceDelta;
+			}
 		}
 		this.requestUpdate();
 	};
